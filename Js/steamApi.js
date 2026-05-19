4e6831200b11100c31524c1b15acaed0e3bab5de
kauessilvas <kauesantosds5@gmail.com>
2026-05-18 22:04:28 -0300

    feat: added stam api request endpoints

--- Js/steamApi.js
+++ Js/steamApi.js
@@ -1,29 +1,29 @@
-const BASE_URL = "https://store.steampowered.com/api/appdetails/";
+const BASE_URL = "https://store.steampowered.com/api/";
 const API_KEY = "1BB5629D80332038A4EBAECACC4E9078";
 const CORS_URL = "https://corsproxy.io/?";
 
-const buildSteamUrl = (params = {}) => {
-  const url = new URL(BASE_URL);
+const buildSteamUrl = (endpoint, params = {}) => {
+    const url = new URL(BASE_URL + endpoint);
 
-  Object.entries(params).forEach(([key, value]) => {
-    url.searchParams.append(key, value);
-  });
+    Object.entries(params).forEach(([key, value]) => {
+        url.searchParams.append(key, value);
+    });
 
-  return CORS_URL + encodeURIComponent(url.toString());
+    return CORS_URL + encodeURIComponent(url.toString());
 };
 
-const fetchSteamApi = async (url) => {
-  try {
-    const response = await fetch(url);
+const fetchSteamApi = async (endpoint, params = {}) => {
+    try {
+        const url = buildSteamUrl(endpoint, params);
 
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
     }
-
-    const result = await response.json();
-
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
 };
